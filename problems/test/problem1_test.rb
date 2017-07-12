f40617408750ddc91771404e2bb127ba34cc99ec
cristicatalan <cristicatalan@gmail.com>
2017-07-12 20:22:06 +0300

    PB-1 Add new failing test

--- problems/test/problem1_test.rb
+++ problems/test/problem1_test.rb
@@ -11,4 +11,10 @@ class Problem1Test < MiniTest::Unit::TestCase
     sum = @problem1.sum_even_numbers(array)
     assert_equal(0, sum)
   end
+
+  def test_given_array_3_5_6_8_11_sum_of_even_numbers_is_14
+    array = [ 3, 5, 6, 8, 11 ]
+    sum = @problem1.sum_even_numbers(array)
+    assert_equal(14, sum)
+  end
 end
