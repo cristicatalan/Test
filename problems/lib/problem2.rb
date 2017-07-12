bfd74a921443d32edc486a5228407867d70468db
cristicatalan <cristicatalan@gmail.com>
2017-07-12 23:02:48 +0300

    PB-2 Make the test pass

--- problems/lib/problem2.rb
+++ problems/lib/problem2.rb
@@ -2,8 +2,8 @@ class Problem2
   def get_largest_two_numbers(array)
     raise "Array must have at least two elements." if array.size < 2
 
-    max1 = 1
-    max2 = 2
+    max1 = array[1]
+    max2 = array[0]
     maximums = [max1, max2]
   end
 end
