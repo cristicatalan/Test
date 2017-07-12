e170ea456aba052cfc8be94228fdf67a7d205808
cristicatalan <cristicatalan@gmail.com>
2017-07-12 22:56:22 +0300

    PB-2 Add new test

--- problems/test/problem2_test.rb
+++ problems/test/problem2_test.rb
@@ -6,14 +6,20 @@ class Problem2Test < MiniTest::Unit::TestCase
     @problem2 = Problem2.new
   end
 
-  def test_array_having_one_element_raises_exception
+  def test_given_array_having_one_element_raises_exception
     array = [1]
     assert_raises("Array must have at least two elements.") {@problem2.get_largest_two_numbers(array)}
   end
 
-  def test_array_having_at_least_two_elements_returns_array_with_two_elements
+  def test_given_array_having_at_least_two_elements_returns_array_with_two_elements
     array = [1, 2, 3]
     maximums = @problem2.get_largest_two_numbers(array)
     assert_equal(2, maximums.size)
+  end
+
+  def test_given_array_1_2_get_maximums_2_1
+    array = [1, 2]
+    maximums = @problem2.get_largest_two_numbers(array)
+    assert(maximums == [2, 1])
   end
 end
