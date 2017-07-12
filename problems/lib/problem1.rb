2e4316ad92431d52fe46886acb9e29b6bf0f7c77
cristicatalan <cristicatalan@gmail.com>
2017-07-12 20:27:52 +0300

    PB-1 Refactor

--- problems/lib/problem1.rb
+++ problems/lib/problem1.rb
@@ -1,13 +1,7 @@
 class Problem1
   def sum_even_numbers(array)
     sum = 0
-
-    array.each do |num|
-      if num%2==0
-        sum+=num
-      end
-    end
-
+    array.each {|num| sum+=num if num.even?}
     sum
   end
 end
