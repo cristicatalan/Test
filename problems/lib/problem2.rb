e7c1275900a5bfb3a379c394a3d3fce165a79b04
cristicatalan <cristicatalan@gmail.com>
2017-07-12 22:46:07 +0300

    PB-2 Refactor

--- problems/lib/problem2.rb
+++ problems/lib/problem2.rb
@@ -2,6 +2,8 @@ class Problem2
   def get_largest_two_numbers(array)
     raise "Array must have at least two elements." if array.size < 2
 
-    maximums = [1, 2]
+    max1 = 1
+    max2 = 2
+    maximums = [max1, max2]
   end
 end
