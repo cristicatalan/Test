76e0160456fcccc42559afc024983c21270b2443
cristicatalan <cristicatalan@gmail.com>
2017-07-13 09:37:15 +0300

    PB-3 Refactor

--- problems/lib/problem3.rb
+++ problems/lib/problem3.rb
@@ -1,11 +1,10 @@
 class Problem3
   def has_duplicate(array)
     for i in (0..array.size-1)
-      if array[array[i] % 5] <= 5
-        array[array[i] % 5] = array[array[i] % 5] + 5
-      else
-        return true
-      end
+      relocated_index = array[i] % 5
+      return true if array[relocated_index] > 5
+
+      array[relocated_index] = array[relocated_index] + 5
     end
 
     return false
