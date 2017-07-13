993b9ecd3d4716cf43e1e1b9885759dba4e352e8
cristicatalan <cristicatalan@gmail.com>
2017-07-13 09:24:22 +0300

    PB-3 Make test pass

--- problems/lib/problem3.rb
+++ problems/lib/problem3.rb
@@ -1,5 +1,13 @@
 class Problem3
   def has_duplicate(array)
-    false
+    for i in (0..array.size-1)
+      if array[array[i] % 5] <= 5
+        array[array[i] % 5] = array[array[i] % 5] + 5
+      else
+        return true
+      end
+    end
+
+    return false
   end
 end
