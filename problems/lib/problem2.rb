c5ad876033b6470ce2fb453a4f5be6a5bffabf5a
cristicatalan <cristicatalan@gmail.com>
2017-07-15 11:13:01 +0300

    PB-2 Make the test pass

--- problems/lib/problem2.rb
+++ problems/lib/problem2.rb
@@ -1,6 +1,6 @@
 class Problem2
   def get_largest_two_numbers(array)
-    raise "Array must have at least two elements." if array.size < 2
+    raise ArgumentError, "Array must have at least two elements." if array.size < 2
 
     if (array[1] > array[0])
       max1 = array[1]
