8b28b7afea012539b24cd08c06c4b32870e93359
cristicatalan <cristicatalan@gmail.com>
2017-07-15 12:33:53 +0300

    PB-2 Refactoring

--- problems/lib/problem2.rb
+++ problems/lib/problem2.rb
@@ -2,18 +2,12 @@ class Problem2
   def get_largest_two_numbers(array)
     raise ArgumentError, "Array must have at least two elements." if array.size < 2
 
-    if (array[1] > array[0])
-      max1 = array[1]
-      max2 = array[0]
-    else
-      max1 = array[0]
-      max2 = array[1]
-    end
+    max1, max2 = get_largest_first_two_numbers(array)
 
     array[2..-1].each do |num|
       next if num <= max2
 
-      if (num>  max1)
+      if num > max1
         max2 = max1
         max1 = num
       else
@@ -22,5 +16,11 @@ class Problem2
     end
 
     [max1, max2]
+  end
+
+  private
+
+  def get_largest_first_two_numbers(array)
+    array[0] > array[1] ? array[0..1] : array[0..1].reverse
   end
 end
